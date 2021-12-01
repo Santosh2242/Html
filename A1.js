5adff39eaf98d5ee4ebf18b659adfe1b61b98f96
Santosh yadav <yadavsantosh6562@gmail.com>
2021-12-01 11:18:42 +0530

    Html

---
+++ A1.js
@@ -0,0 +1,3 @@
+
+  var name=  prompt("Name","enter a value");
+    console.log("Name is : "+name);
---
+++ A2.js
@@ -0,0 +1,3 @@
+var num1 = parseInt(prompt("Enter First No : "))
+var num2 = Numb76er(prompt("Enter Second No : "))
+alert("Result is : "+(num1+num2))
